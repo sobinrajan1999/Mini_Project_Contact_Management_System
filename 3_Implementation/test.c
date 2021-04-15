070c00ced6d970668c55d93c1fc0ec8ae936dc47
sobinrajan1999 <42982153+sobinrajan1999@users.noreply.github.com>
2021-04-15 12:11:32 +0530

    Update test.c

--- 3_Implementation/test.c
+++ 3_Implementation/test.c
@@ -8,19 +8,8 @@ void tearDown(){}
 
 void test_addcontact(void){
     TEST_ASSERT_EQUAL(1,add_contact("Sobin","7814174948","ludhiana","sobin@gmail.com"));
-    TEST_ASSERT_EQUAL(0,add_contact("Sobin","8874598234","ludhiana"," "));
 }
                     
-void test_search_contact(void){
-    TEST_ASSERT_EQUAL(1,search_contact("Annamma"));
-    TEST_ASSERT_EQUAL(0,search_contact("Vishwas"));
-}
-                      
-void test_edit_delete_contact(void){
-    TEST_ASSERT_EQUAL(1,edit_delete_contact("Annamma"));
-    TEST_ASSERT_EQUAL(0,edit_delete_contact("mammad"));
-}
-
 void test_search_result(void){
     TEST_ASSERT_EQUAL(1,search_result("annamma"));
     TEST_ASSERT_EQUAL(0,search_result("Vishwas"));
@@ -35,8 +24,6 @@ int main(void){
     UNITY_BEGIN();
 
     RUN_TEST(test_addcontact);
-    RUN_TEST(test_search_contact);
-    RUN_TEST(test_edit_delete_contact);
     RUN_TEST(test_search_result);
     RUN_TEST(test_compare_name);
 
